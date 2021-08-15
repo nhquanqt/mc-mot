fc5abff6e2f1f2a7758b5a7824f1a013452a4811
wan2000 <hoangquan.qti@gmail.com>
2021-08-15 11:25:54 +0700

    remove train reid

--- main.py
+++ main.py
@@ -1,51 +1,23 @@
 import os, time
 
 import torch
-from torchvision.transforms.transforms import Pad, RandomCrop
-
-from reid.model.model import Model
-from reid.train import train_model
 
 from torchvision import transforms
-
-from torch.optim import Adam
-from reid.model.triplet_loss import TripletLoss
-from torch.optim.lr_scheduler import StepLR
-from reid.dataset.market1501_trainval import Market1501TrainVal
 
 from mc_mot.mot import MultipleObjectTracker
 from mc_mot.dataset.prw import PRW
 
+import torchreid
 import cv2
 
 import matplotlib.pyplot as plt
 import networkx
 
-def test_reid():
-    model = Model(last_conv_stride=1)
-
-    optimizer = Adam(model.parameters(), lr=0.0002, weight_decay=0.0005)
-    scheduler = StepLR(optimizer, step_size=40, gamma=0.1)
-    criterion = TripletLoss(0.3)
-
-    transform_trainval = transforms.Compose([
-        transforms.Resize([256, 128]),
-        transforms.Pad(10),
-        transforms.RandomCrop([256, 128]),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-
-    dataloader = Market1501TrainVal('/home/wan/datasets/Market-1501-v15.09.15', transform=transform_trainval, batch_size=2)
-
-    model = train_model(model, optimizer, criterion, scheduler, dataloader)
-
 def test_mc_mot():
-    feature_extractor = Model(last_conv_stride=1)
-
-    checkpoint = torch.load('reid/data/model_weight.pth', map_location='cpu')
-    feature_extractor.load_state_dict(checkpoint)
+    feature_extractor = torchreid.models.build_model(
+        name='osnet_x1_0',
+        num_classes=1000
+    )
 
     mot = MultipleObjectTracker(feature_extractor)
 
@@ -57,7 +29,7 @@ def test_mc_mot():
 
     prw = PRW('/home/wan/datasets/PRW-v16.04.20', transform)
 
-    for t_step in range(10000):
+    for t_step in range(0, 10000):
         input_t, frame, bboxes = prw[t_step]
 
         if len(input_t) == 0:
