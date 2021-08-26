c2082f772ee6dc16a53bf1d36fc8adc550dd3fce
wan2000 <hoangquan.qti@gmail.com>
2021-08-26 16:18:55 +0700

    use new STGAT

--- mc_mot/mot.py
+++ mc_mot/mot.py
@@ -1,11 +1,11 @@
 import numpy as np
 import torch
 
-from gat.models import GAT
-from mc_mot.layers import StructuralAttentionLayer
-from mc_mot.layers import TemporalAttentionLayer
-
 import networkx as nx
+
+from stgat.models import STGAT
+
+import torch.nn.functional as F
 
 class MultipleObjectTracker():
     def __init__(self, feature_extractor, feature_dim=512, hidden_dim=128, embedding_dim=8):
@@ -23,33 +23,20 @@ class MultipleObjectTracker():
 
         self.G = nx.Graph()
 
-        self.gat = GAT(feature_dim, 8, 512, 0.1, 0.2, 8)
-        self.gat.load_state_dict(torch.load('gat/data/gat_weight.pth', map_location='cpu'))
-        self.gat.eval()
+        self.stgat = STGAT()
+        self.stgat.load_state_dict(torch.load('stgat/data/model_weight_last.pth', map_location='cpu'))
+        self.stgat.eval()
 
-        sal_heads = feature_dim // hidden_dim
-        self.SAL = StructuralAttentionLayer(feature_dim, hidden_dim, hidden_dim, sal_heads)
-        self.SAL.load_state_dict(torch.load('mc_mot/data/SAL_weight_v2.pth', map_location='cpu'))
-        self.SAL.eval()
+        self.h_adj = [torch.zeros(1, 1) for _ in range(3)]
+        self.h_features = [torch.zeros(1, 512) for _ in range(3)]
 
-        tal_heads = hidden_dim // embedding_dim
-        self.TAL = TemporalAttentionLayer(tal_heads, hidden_dim, embedding_dim)
-        self.TAL.load_state_dict(torch.load('mc_mot/data/TAL_weight_v2.pth', map_location='cpu'))
-        self.TAL.eval()
-
-        self.h_adj = [None for _ in range(3)]
-        self.h_features = [torch.zeros(1, self.hidden_dim) for _ in range(3)]
-
-    def __call__(self, x, link_threshold=0.8, use_stgat=True):
+    def __call__(self, x, link_threshold=0.8):
         features = self.feature_extractor(x).detach()
         n_new_nodes = features.size(0)
 
         self.add_nodes(features)
 
-        if use_stgat:
-            infer_features = self.stgat_infer().detach()
-        else:
-            infer_features = self.gat_infer().detach()
+        infer_features = self.stgat_infer().detach()
 
         track_ids = [-1 for _ in range(n_new_nodes)]
         max_sim = [0 for _ in range(n_new_nodes)]
@@ -109,13 +96,13 @@ class MultipleObjectTracker():
             if linked_node_id[i] != -1:
                 self.G.add_edge(node_id, linked_node_id[i])
 
-            # for j in range(self.n_nodes - n_new_nodes):
-            #     if self.G.nodes[j]['track_id'] == track_ids[i] and j != linked_node_id[i]:
-            #         sim = torch.sigmoid(
-            #             torch.mm(infer_features[node_id].unsqueeze(0), 
-            #                     infer_features[j].unsqueeze(0).T).view(-1))
-            #         if sim > link_threshold:
-            #             self.G.add_edge(node_id, j)
+            for j in range(self.n_nodes - n_new_nodes):
+                if self.G.nodes[j]['track_id'] == track_ids[i] and j != linked_node_id[i]:
+                    sim = torch.sigmoid(
+                        torch.mm(infer_features[node_id].unsqueeze(0), 
+                                infer_features[j].unsqueeze(0).T).view(-1))
+                    if sim > link_threshold:
+                        self.G.add_edge(node_id, j)
 
         return track_ids
 
@@ -129,49 +116,22 @@ class MultipleObjectTracker():
 
             self.n_nodes += 1
 
-    def gat_infer(self):
-        adj = nx.to_scipy_sparse_matrix(self.G, format='coo')
-
-        indices = np.vstack((adj.row, adj.col))
-        values = adj.data
-
-        i = torch.LongTensor(indices)
-        v = torch.FloatTensor(values)
-        shape = adj.shape
-
-        adj = torch.sparse_coo_tensor(i, v, shape)
-        
-        return self.gat(torch.cat(self.node_features).view(-1, self.feature_dim), adj.to_dense())
-        
     def stgat_infer(self):
-        self.h_features.append(self.node_features.copy())
+        self.h_features.append(torch.cat(self.node_features).view(-1, 512))
         self.h_adj.append(torch.tensor(nx.to_numpy_matrix(self.G), dtype=torch.float32))
 
         if len(self.h_adj) > 3:
             self.h_features.pop(0)
             self.h_adj.pop(0)
 
-        h = []
+        for i in range(2):
+            d = self.h_features[-1].size(0) - self.h_features[i].size(0)
+            self.h_features[i] = F.pad(self.h_features[i], (0, 0, 0, d))
+            self.h_adj[i] = F.pad(self.h_adj[i], (0, d, 0, d))
 
-        for i in range(len(self.h_adj)):
-            adj = self.h_adj[i]
+        in_features = torch.stack(self.h_features)
+        in_adj = torch.stack(self.h_adj)
 
-            if adj is not None:
-                features = torch.cat(self.h_features[i]).view(-1, self.feature_dim)
-                infered_features = self.SAL(features, adj)
-            else:
-                infered_features = torch.tensor(self.h_features[i])
-
-            if infered_features.size(0) < self.n_nodes:
-                infered_features = torch.cat([
-                    infered_features, 
-                    torch.zeros(self.n_nodes - infered_features.size(0), self.hidden_dim)
-                ])
-
-            h.append(infered_features)
-
-        input_t = torch.stack(h).transpose(0,1)
-
-        embedding = self.TAL(input_t)
+        embedding = self.stgat(in_features, in_adj)
 
         return embedding[:, -1]
